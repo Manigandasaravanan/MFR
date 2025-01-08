7b0944994edd10f5615583bbd3f3a22cb50bb6e1
Manigandasaravanan <rsaranit@gmail.com>
2025-01-08 11:53:58 +0400

    framework created

--- Package.swift
+++ Package.swift
@@ -5,20 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "MFRFramework",
+    platforms: [
+      .macOS(.v12), .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MFRFramework",
             targets: ["MFRFramework"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MFRFramework"),
-        .testTarget(
-            name: "MFRFrameworkTests",
-            dependencies: ["MFRFramework"]
-        ),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "MFRFramework",
+            path: "./Sources/MFRFramework.xcframework")
     ]
 )
---
+++ Sources/MFRFramework.xcframework/ios-arm64_x86_64-simulator/MFRFramework.framework/Headers/MFRFramework.h
@@ -0,0 +1,18 @@
+//
+//  MFRFramework.h
+//  MFRFramework
+//
+//  Created by Mani on 08/01/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MFRFramework.
+FOUNDATION_EXPORT double MFRFrameworkVersionNumber;
+
+//! Project version string for MFRFramework.
+FOUNDATION_EXPORT const unsigned char MFRFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MFRFramework/PublicHeader.h>
+
+
